546d5d3dbd75eccd22c891b23d19b40d237c7f9a
xoxrainie <xoxrainie@gmail.com>
2024-06-29 12:42:21 +0300

    add Image model

--- account/forms.py
+++ account/forms.py
@@ -21,14 +21,29 @@ class UserRegistrationForm(forms.ModelForm):
             raise forms.ValidationError('Пароли не совпадают')
         return cd['password2']
     
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        if User.objects.filter(email=data).exists():
+            raise forms.ValidationError('Пользователь с таким email уже существует.')
+        return data
 
 class UserEditForm(forms.ModelForm):
+        
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email']
 
+    
+    def clean_email(self):
+        data = self.cleaned_data
+        qs = User.objects.exclude(id=self.instance.id).filter(email=data)
+        if qs.exists():
+            raise forms.ValidationError('Пользователь с таким email уже существует')
+        return data
+
 
 class ProfileEditForm(forms.ModelForm):
+
     class Meta:
         model = Profile
         fields = ['date_of_birth', 'photo']
--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'images.apps.ImagesConfig',
 ]
 
 MIDDLEWARE = [
