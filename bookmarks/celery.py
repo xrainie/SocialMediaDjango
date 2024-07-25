36ced1ccf799f5ac5845d8b9152824f6fb608335
xoxrainie <xoxrainie@gmail.com>
2024-07-25 04:26:41 +0300

    rename

--- bookmarks/celery.py
+++ bookmarks/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookmarks.settings')
-app = Celery('bookmarks')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'marks.settings')
+app = Celery('marks')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
